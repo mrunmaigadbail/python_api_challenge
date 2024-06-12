42c2375c8d1674762e87ca2b9fb45162f1af1d46
mrunmaigadbail <mrunmaigadbail@gmail.com>
2024-06-12 19:58:27 -0400

    sumbit

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "d85cab6f5865fee01f5eddfd891ef779"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "c6e707349acb4cfda1bb149ad3823883"
